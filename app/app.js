c98f4fae01adab14ef6c9bd8fbfbf6ce808efe73
coomy <coomysky@gmail.com>
2017-03-28 11:58:15 +0800

    Update Home Page

---
+++ app/app.js
@@ -0,0 +1,43 @@
+import angular from 'angular';
+import 'imhere-angular-wizard';
+
+import '../style/app.css';
+
+let app = () => {
+  return {
+    template: require('./app.html'),
+    controller: 'AppCtrl',
+    controllerAs: 'app'
+  }
+};
+
+class AppCtrl {
+  /*@ngInject*/
+  constructor($log) {
+    this.$log = $log;
+    this.checkPass = false;
+    this.url = 'https://github.com/preboot/angular-webpack';
+  }
+  finish() {
+    this.$log.debug('finish');
+  }
+  check(){
+    return !this.checkPass;
+  }
+  setCheckPass(){
+    this.checkPass = true;
+  }
+  lockCheckPass(){
+    this.checkPass = false;
+  }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [
+  'imhere-angular-wizard',
+])
+  .directive('app', app)
+  .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
